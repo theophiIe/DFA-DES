795d88c623fe3a46fe5234953137572764bdf8f5
theophiIe <theomolinatti@gmail.com>
2022-02-26 21:43:54 +0100

    Add expansion and initial permutation

---
+++ header/des.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+
+//
+void print_binary_64(__uint64_t number);
+
+//
+void print_binary_32(__uint32_t number);
+
+//
+__uint64_t initial_permutation(__uint64_t value);
+
+//
+__uint64_t expansion(__uint32_t R);
+
+//
+void get_L16_R16(__uint64_t value, __uint32_t *L16, __uint32_t *R16);
