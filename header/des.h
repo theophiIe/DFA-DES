a3665895d76ecca168713ce49c2373108ae017c4
theophiIe <theomolinatti@gmail.com>
2022-02-26 23:24:27 +0100

    permutation_inv

--- header/des.h
+++ header/des.h
@@ -15,4 +15,7 @@ __uint64_t initial_permutation(__uint64_t value);
 __uint64_t expansion(__uint32_t R);
 
 //
+__uint32_t permutation_inv(__uint32_t R);
+
+//
 void get_L16_R16(__uint64_t value, __uint32_t *L16, __uint32_t *R16);
