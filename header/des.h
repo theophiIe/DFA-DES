13ffe3056c44fd3612630db5cbcd3dd9cefc9a0e
theophiIe <theomolinatti@gmail.com>
2022-03-04 00:42:53 +0100

    Ajout des fonctions pc1_inv, pc2_inv modif de get_LetR en deux fonctions + correction erreur dans permutation_inv

--- header/des.h
+++ header/des.h
@@ -3,12 +3,6 @@
 #include <stdio.h>
 
 //
-void print_binary_64(__uint64_t number);
-
-//
-void print_binary_32(__uint32_t number);
-
-//
 __uint64_t initial_permutation(__uint64_t value);
 
 //
@@ -18,4 +12,13 @@ __uint64_t expansion(__uint32_t R);
 __uint32_t permutation_inv(__uint32_t R);
 
 //
-void get_L16_R16(__uint64_t value, __uint32_t *L16, __uint32_t *R16);
+__uint64_t pc1_inv(__uint64_t value);
+
+//
+__uint64_t pc2_inv(__uint64_t value);
+
+//
+__uint32_t get_L(__uint64_t value);
+
+//
+__uint32_t get_R(__uint64_t value);
